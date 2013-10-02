1885cdcc9e3c5d4ea8e8f711fb2f8cc47be1c376
Thant <t@ttkz.me>
2013-10-02 13:28:31 +0630

    iOS7 fix

--- src/Three20UI/Sources/TTTableActivityItemCell.m
+++ src/Three20UI/Sources/TTTableActivityItemCell.m
@@ -65,7 +65,11 @@
 - (void)layoutSubviews {
   [super layoutSubviews];
 
-  UITableView* tableView = (UITableView*)self.superview;
+  UITableView *tableView = (UITableView*)self.superview;
+  while ([tableView isKindOfClass:[UITableView class]] == NO) {
+      tableView = (UITableView*)tableView.superview;
+  }
+
   if (tableView.style == UITableViewStylePlain) {
     _activityLabel.frame = self.contentView.bounds;
 
